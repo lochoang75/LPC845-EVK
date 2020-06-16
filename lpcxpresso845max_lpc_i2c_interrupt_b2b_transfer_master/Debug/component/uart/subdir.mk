fe580a00835ddbb48eda7924ba6f9f7dd2ea4ed9
ynguyen0404 <ynguyen0404@gmail.com>
2020-06-16 17:20:41 +0700

    update I2C_Interrupt_SensorLight BH1750

---
+++ lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master/Debug/component/uart/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../component/uart/miniusart_adapter.c 
+
+OBJS += \
+./component/uart/miniusart_adapter.o 
+
+C_DEPS += \
+./component/uart/miniusart_adapter.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+component/uart/%.o: ../component/uart/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DCPU_LPC845M301JBD64 -DCPU_LPC845M301JBD64_cm0plus -DCPU_LPC845 -DCPU_LPC845M301JHI48 -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\board" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\source" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\drivers" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\device" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\CMSIS" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\component\uart" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\utilities" -O0 -fno-common -g3 -Wall -c  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master/Debug/source/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/i2c_interrupt_b2b_transfer_master.c \
+../source/mtb.c \
+../source/semihost_hardfault.c 
+
+OBJS += \
+./source/i2c_interrupt_b2b_transfer_master.o \
+./source/mtb.o \
+./source/semihost_hardfault.o 
+
+C_DEPS += \
+./source/i2c_interrupt_b2b_transfer_master.d \
+./source/mtb.d \
+./source/semihost_hardfault.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DCPU_LPC845M301JBD64 -DCPU_LPC845M301JBD64_cm0plus -DCPU_LPC845 -DCPU_LPC845M301JHI48 -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\board" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\source" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\drivers" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\device" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\CMSIS" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\component\uart" -I"C:\Users\PC-001\Documents\MCUXpressoIDE_11.1.1_3241\workspace\lpcxpresso845max_lpc_i2c_interrupt_b2b_transfer_master\utilities" -O0 -fno-common -g3 -Wall -c  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
